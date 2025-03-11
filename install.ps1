f97c082952c4f9487bf6642a05692a50a68db08d
Talin Paul <talinpaul@outlook.com>
2025-03-11 16:46:23 +0530

    updated the install script

--- install.ps1
+++ install.ps1
@@ -1,6 +1,47 @@
 # Ensure PowerShell stops on errors
 $ErrorActionPreference = "Stop"
 
+# Check if Scoop is installed
+Write-Host "Checking for Scoop installation..."
+if (-not (Get-Command scoop -ErrorAction SilentlyContinue)) {
+    Write-Host "❌ Scoop is not installed. Trying to install Scoop and try again."
+    # Set execution policy to allow running scripts
+    Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+    # Download and execute Scoop installer
+    Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+} else {
+    Write-Output "Scoop is already installed."
+}
+
+# Update Scoop to the latest version
+Write-Host "Updating Scoop..."
+scoop update
+
+# Function to install a package using Scoop if it's not already installed
+function Install-Package-If-Needed {
+    param (
+        [string]$packageName
+    )
+    if (-not (scoop list $packageName -q)) {
+        Write-Host "Installing $packageName..."
+        scoop install $packageName
+    } else {
+        Write-Host "$packageName is already installed."
+    }
+}
+
+
+# Install Python using Scoop
+Install-Package-If-Needed "python"
+
+# Install Mozilla Firefox using Scoop
+Install-Package-If-Needed "firefox"
+
+# Install GeckoDriver using Scoop
+Install-Package-If-Needed "geckodriver"
+
+Write-Output "Installation of Mozilla Firefox and GeckoDriver is complete."
+
 # Check if Python is installed
 Write-Host "Checking for Python installation..."
 if (-not (Get-Command python -ErrorAction SilentlyContinue)) {
